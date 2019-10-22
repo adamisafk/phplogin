eae18422590c63a3e117c4e8dda81f5bd9d88cea
Adam Howells <adamisafk@gmail.com>
2019-10-22 16:11:15 +0100

    Modify database handling to use Facade pattern

--- profile.php
+++ profile.php
@@ -6,22 +6,22 @@ if (!isset($_SESSION['loggedin'])) {
 	header('Location: index.html');
 	exit();
 }
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'phplogin';
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_errno()) {
-	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
-// We don't have the password or email info stored in sessions so instead we can get the results from the database.
-$stmt = $con->prepare('SELECT password, email FROM accounts WHERE id = ?');
-// In this case we can use the account ID to get the account info.
-$stmt->bind_param('i', $_SESSION['id']);
-$stmt->execute();
-$stmt->bind_result($password, $email);
-$stmt->fetch();
-$stmt->close();
+// $DATABASE_HOST = 'localhost';
+// $DATABASE_USER = 'root';
+// $DATABASE_PASS = '';
+// $DATABASE_NAME = 'phplogin';
+// $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+// if (mysqli_connect_errno()) {
+// 	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+// }
+// // We don't have the password or email info stored in sessions so instead we can get the results from the database.
+// $stmt = $con->prepare('SELECT password, email FROM accounts WHERE id = ?');
+// // In this case we can use the account ID to get the account info.
+// $stmt->bind_param('i', $_SESSION['id']);
+// $stmt->execute();
+// $stmt->bind_result($password, $email);
+// $stmt->fetch();
+// $stmt->close();
 ?>
 
 <!DOCTYPE html>
@@ -50,12 +50,8 @@ $stmt->close();
 						<td><?=$_SESSION['name']?></td>
 					</tr>
 					<tr>
-						<td>Password:</td>
-						<td><?=$password?></td>
-					</tr>
-					<tr>
 						<td>Email:</td>
-						<td><?=$email?></td>
+						<td><?=$_SESSION['email']?></td>
 					</tr>
 				</table>
 			</div>
